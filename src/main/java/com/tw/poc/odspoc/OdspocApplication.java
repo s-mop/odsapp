2ae42f0c27bb6daaaf064e1b0f8d2fc23ee3a05c
s-mop <xinyuan47@qq.com>
2022-06-27 12:45:33 +0800

    Confirmed UNACK

--- src/main/java/com/tw/poc/odspoc/OdspocApplication.java
+++ src/main/java/com/tw/poc/odspoc/OdspocApplication.java
@@ -1,16 +1,12 @@
 package com.tw.poc.odspoc;
 
+import com.mongodb.MongoClientSettings;
 import com.mongodb.WriteConcern;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.WriteConcernResolver;
-import org.springframework.data.mongodb.core.WriteResultChecking;
-import org.springframework.data.mongodb.core.convert.MongoConverter;
 
 @SpringBootApplication
 @Configuration
@@ -22,18 +18,9 @@ public class OdspocApplication {
 	}
 
 	@Bean
-	MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDbFactory, MongoConverter converter) {
-		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
-		log.debug("Setting WriteConcern statically to ACKNOWLEDGED");
-		mongoTemplate.setWriteConcern(WriteConcern.UNACKNOWLEDGED);
-		// Version 2: provide a WriteConcernResolver, which is called for _every_ MongoAction
-		// which might degrade performance slightly (not tested)
-		// and is very flexible to determine the value
-		mongoTemplate.setWriteConcernResolver(action -> {
-			log.debug("Action {} called on collection {} for document {} with WriteConcern.MAJORITY. Default WriteConcern was {}", action.getMongoActionOperation(), action.getCollectionName(), action.getDocument(), action.getDefaultWriteConcern());
-			return WriteConcern.UNACKNOWLEDGED;
-		});
-		mongoTemplate.setWriteResultChecking(WriteResultChecking.EXCEPTION);
-		return mongoTemplate;
+	MongoClientSettings mongoClientSettings() {
+		MongoClientSettings.Builder builder = MongoClientSettings.builder();
+		builder.writeConcern(WriteConcern.UNACKNOWLEDGED);
+		return builder.build();
 	}
 }
