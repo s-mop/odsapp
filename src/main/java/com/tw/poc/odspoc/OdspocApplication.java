9e92ce9096c9b89f4ba07c1478f7304e61a240d4
s-mop <xinyuan47@qq.com>
2022-06-27 12:51:18 +0800

    fix mongo config

--- src/main/java/com/tw/poc/odspoc/OdspocApplication.java
+++ src/main/java/com/tw/poc/odspoc/OdspocApplication.java
@@ -5,8 +5,11 @@ import com.mongodb.WriteConcern;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.autoconfigure.mongo.MongoPropertiesClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @Configuration
@@ -22,5 +25,10 @@ public class OdspocApplication {
 		MongoClientSettings.Builder builder = MongoClientSettings.builder();
 		builder.writeConcern(WriteConcern.UNACKNOWLEDGED);
 		return builder.build();
+	}
+	@Bean
+	MongoPropertiesClientSettingsBuilderCustomizer mongoPropertiesCustomizer(MongoProperties properties,
+																			 Environment environment) {
+		return new MongoPropertiesClientSettingsBuilderCustomizer(properties, environment);
 	}
 }
